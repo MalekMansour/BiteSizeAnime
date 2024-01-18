2466495599e166db0a4a8880f08aa14200cd4314
Malek Mansour <138655005+MalekMansour@users.noreply.github.com>
2024-01-18 08:36:03 -0700

    added redirectToRandomAnime function

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function redirectToWatch(animePage) {
     window.location.href = animePage;
 }
+
+function redirectToRandomAnime() {
+    const animePages = ['tpn.html', 'spyxfamily.html', 'tokyoghoul.html', 'erased.html', 'prison_school.html', 'umaru.html', 'death_note.html'];
+    const randomIndex = Math.floor(Math.random() * animePages.length);
+    window.location.href = animePages[randomIndex];
+}
